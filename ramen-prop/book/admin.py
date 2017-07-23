e4b190b44410c21ac66fbc09649f5fcd3aa4d598
MunsuPark <mspark@8percent.kr>
2017-07-23 19:32:00 +0900

    ADD: 모델 및 어드민 작업.

---
+++ ramen-prop/book/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django_extensions.admin import ForeignKeyAutocompleteAdmin
+
+from .models import Author
+from .models import Book
+from .models import User
+from .models import BookMark
+from .models import Comment
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class AuthorAdmin(ForeignKeyAutocompleteAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookMark)
+class BookMarkAdmin(ForeignKeyAutocompleteAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(ForeignKeyAutocompleteAdmin):
+    pass
+
+
---
+++ ramen-prop/book/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+from django_extensions.db.models import TimeStampedModel
+
+
+class Author(TimeStampedModel):
+    """저자"""
+    name = models.CharField('이름', db_index=True, max_length=30)
+    birth_date = models.DateField('출생일', null=True, blank=True)
+
+    def __str__(self):
+        return '{}_{}'.format(self.name, self.birth_date)
+
+
+class Book(TimeStampedModel):
+    """도서"""
+    author = models.ForeignKey(Author, related_name='books')
+    title = models.CharField('제목', db_index=True, max_length=100)
+    # TODO - 표준 언어코드로(https://ko.wikipedia.org/wiki/ISO_639#.EC.96.B8.EC.96.B4_.EB.B6.80.ED.98.B8_.EB.AA.A9.EB.A1.9D)
+    language = models.CharField('언어', max_length=10, blank=True)
+    publisher = models.CharField('출판사', max_length=50, blank=True)
+    date_of_publication = models.DateField('출판일', null=True, blank=True)
+    cover_img = models.ImageField('표지그림', blank=True)
+
+    def __str__(self):
+        return '{}_{}'.format(self.title, self.author.name)
+
+
+class User(TimeStampedModel):
+    """사용자"""
+    name = models.CharField('이름', max_length=30)
+    email = models.EmailField('이메일', db_index=True, unique=True)
+
+    def __str__(self):
+        return '{}_{}'.format(self.name, self.email)
+
+
+class BookMark(TimeStampedModel):
+    """책갈피"""
+    reader = models.ForeignKey(User, related_name='bookmarks')
+    book = models.ForeignKey(Book, related_name='bookmarks')
+    page = models.IntegerField('페이지')
+
+    def __str__(self):
+        return '{}_{}'.format(self.reader.email, self.book.title)
+
+
+class Comment(TimeStampedModel):
+    """서평"""
+    writer = models.ForeignKey(User, related_name='comments')
+    book = models.ForeignKey(Book, related_name='comments')
+    contents = models.CharField('내용', max_length=500)
+
+    def __str__(self):
+        return '{}_{}'.format(self.writer.email, self.book.title)
+
